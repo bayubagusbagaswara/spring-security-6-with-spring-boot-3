0e72ec686eb4cf89b2f59be730b31d20cfff0cd6
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-07 19:40:13 +0700

    initial commit

---
+++ src/test/java/com/bayu/springboot/SpringbootSecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
